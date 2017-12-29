15dfe5872adbd04f95671fd527aa83f9d36256a4
Bruce Downs <bruce.downs@jivesoftware.com>
2017-12-29 12:59:08 -0800

    solution to problem 10a

--- 10/main.go
+++ 10/main.go
@@ -13,22 +13,20 @@ The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 Find the sum of all the primes below two million.
 */
 
-func isPrime(i int) bool {
-	for j := 2; j <= int(math.Floor(math.Sqrt(float64(i)))); j++ {
+func appendPrime(in []int, i int) []int {
+	for j := 2; j <= int(math.Sqrt(float64(i))); j++ {
 		if i%j == 0 {
-			return false
+			return in
 		}
 	}
 
-	return true
+	return append(in, i)
 }
 
 func main() {
 	primes := make([]int, 0)
 	for i := 2; i < 2000000; i++ {
-		if isPrime(i) {
-			primes = append(primes, i)
-		}
+		primes = appendPrime(primes, i)
 	}
 
 	var sum int
