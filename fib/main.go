11fab9b2c3507c94c50777c253061dd0fb6006d0
Bruce Downs <bruce.downs@jivesoftware.com>
2017-12-30 18:12:47 -0800

    solution 2 to recursive fibonacci
    * use cache for previous calculated

--- fib/main.go
+++ fib/main.go
@@ -2,16 +2,24 @@ package main
 
 import "log"
 
-func fib(n int) int {
+var cache = make(map[int]uint64)
+
+func fib(n int) uint64 {
 	if n < 2 {
 		return 1
 	}
 
-	return fib(n-1) + fib(n-2)
+	if c, ok := cache[n]; ok {
+		return c
+	}
+
+	res := fib(n-1) + fib(n-2)
+	cache[n] = res
+	return res
 }
 
 func main() {
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 50; i++ {
 		log.Printf("fib(%d): %d", i, fib(i))
 	}
 }
